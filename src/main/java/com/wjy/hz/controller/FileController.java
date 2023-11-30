fceba982c5e0d7456d186a7199fab83a8fca5c9a
jingyi.wang <jingyi.wang@dbappsecurity.com.cn>
2023-11-30 13:48:45 +0800

    pic upload.

---
+++ src/main/java/com/wjy/hz/controller/FileController.java
@@ -0,0 +1,58 @@
+package com.wjy.hz.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class FileController {
+
+    @PostMapping("/api/upload/image")
+    public ResponseEntity<String> uploadImage(@RequestParam("wangeditor-uploaded-image") MultipartFile image) {
+        JSONObject responseJson = new JSONObject();
+        try {
+            // 这里添加保存文件的逻辑，假设保存后得到了一个可访问的文件URL
+            String fileUrl = saveImage(image);
+
+            // 构建返回的JSON数据
+            JSONObject dataJson = new JSONObject();
+            dataJson.put("url", fileUrl);
+            dataJson.put("alt", "Image description"); // 可以是动态数据
+            dataJson.put("href", "Image link"); // 可以是动态数据
+
+            responseJson.put("errno", 0);
+            responseJson.put("data", dataJson);
+        } catch (Exception e) {
+            // 发生错误时的逻辑
+            responseJson.put("errno", 1);
+            responseJson.put("message", "上传失败，原因：" + e.getMessage());
+        }
+
+        return ResponseEntity.ok(responseJson.toJSONString());
+    }
+
+    private String saveImage(MultipartFile image) throws IOException {
+        // 处理图片保存逻辑
+        String filePath;
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            // Windows开发环境路径
+            filePath = "C:\\data\\" + image.getOriginalFilename();
+        } else {
+            // 生产环境路径
+            filePath = "/webapp/images/uploads/" + image.getOriginalFilename();
+            //System.out.println("Original filename: " + image.getOriginalFilename());
+            File destination = new File(filePath);
+            image.transferTo(destination);
+        }
+
+        // 返回图片的访问URL
+        return "http://www.hz-study-system.com/wjy/images/" + image.getOriginalFilename();
+    }
+
+}
