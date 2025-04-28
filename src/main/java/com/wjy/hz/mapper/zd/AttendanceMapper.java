7ad5b28b2c619e30fe50599ec437bd4dd0f2ea83
jingyi.wang <jingyi.wang@jingyi.wang>
2025-04-28 14:40:37 +0800

    zd页面

--- src/main/java/com/wjy/hz/mapper/zd/AttendanceMapper.java
+++ src/main/java/com/wjy/hz/mapper/zd/AttendanceMapper.java
@@ -8,10 +8,11 @@ import java.util.List;
 
 @Mapper
 public interface AttendanceMapper {
-    // AttendanceMapper.java
-    @Select("SELECT r.id, p.name, r.type, r.attendance " +
+
+    @Select("SELECT r.id, p.name, p.avatar, r.type, r.attendance, h.hname, h.havatar, r.result, r.kda " +
             "FROM zd_person_record r " +
             "JOIN zd_person p ON r.person = p.id " +
+            "LEFT JOIN zd_heros h ON r.hero = h.id " +
             "LIMIT #{offset}, #{size}")
     List<AttendanceDto> getAttendanceList(int offset, int size);
 }
--- src/main/java/com/wjy/hz/model/dto/zd/AttendanceDto.java
+++ src/main/java/com/wjy/hz/model/dto/zd/AttendanceDto.java
@@ -9,10 +9,20 @@ public class AttendanceDto {
 
     private int id;
 
-    private String name;
+    private String name; // user's name
+
+    private String avatar;// users' avatar
 
     private String type;
 
     private LocalDateTime attendance;
+
+    private String hname; // hero's name
+
+    private String havatar; // hero's avatar
+
+    private String result; // match's result
+
+    private String kda; // user's kda
 
 }
